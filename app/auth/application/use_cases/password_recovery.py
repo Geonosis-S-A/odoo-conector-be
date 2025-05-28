d5ce207bbab834525493269b237ff865d0bfacb0
Federico Mancilla <fedemancilla00@gmail.com>
2025-05-28 15:21:27 -0300

    recovery request otp refactorizado con las nuevas clases

--- app/auth/application/use_cases/password_recovery.py
+++ app/auth/application/use_cases/password_recovery.py
@@ -8,10 +8,12 @@ from app.auth.application.dto.password_recovery import (
     VerifyOTPDTO,
     ResetPasswordDTO,
 )
-from app.auth.domain.repositories import UserRepository
+from app.auth.application.services.otp_service import OTPService
+from app.auth.domain.repositories import OTPRepository
 from app.auth.infra.db.models import OTPModel
 from app.auth.infra.email_service import EmailService
 from app.auth.infra.password_service import PasswordService
+from app.users.domain.repositories import UserRepository
 
 
 class PasswordRecoveryUseCase:
@@ -20,31 +22,25 @@ class PasswordRecoveryUseCase:
         user_repository: UserRepository,
         email_service: EmailService,
         password_service: PasswordService,
+        otp_repository: OTPRepository,
     ):
         self.user_repository = user_repository
         self.email_service = email_service
         self.password_service = password_service
-
-    def generate_otp(self) -> str:
-        return "".join(random.choices(string.digits, k=6))
+        self.otp_repository = otp_repository
+        self.otp_service = OTPService()
 
     async def request_otp(self, dto: RequestOTPDTO) -> bool:
-        user = await self.user_repository.get_by_email(dto.email)
+        user = self.user_repository.get_by_email(dto.email)
         if not user or user.id is None:
             return False
 
         # Generar código OTP
-        otp_code = self.generate_otp()
-        expires_at = datetime.utcnow() + timedelta(minutes=15)
-
-        # Guardar OTP en la base de datos
-        otp = OTPModel(
-            user_id=int(user.id), code=otp_code, expires_at=expires_at, is_used=False
-        )
-        await self.user_repository.save_otp(otp)
+        new_otp = self.otp_service.create_otp(int(user.id))
+        await self.otp_repository.save(new_otp)
 
         # Enviar email con el código
-        await self.email_service.send_otp_email(dto.email, otp_code)
+        await self.email_service.send_otp_email(dto.email, new_otp.code)
         return True
 
     async def verify_otp(self, dto: VerifyOTPDTO) -> bool:
