634d1441e10362205f67b1c5f908d48ce4fb10b6
FelipePaparelo <felipe.paparelo@geonosis.com>
2025-07-08 12:52:36 -0300

    se quita la url, hay que arreglar problemas

--- app/main.py
+++ app/main.py
@@ -70,7 +70,6 @@ app.add_middleware(
         "http://localhost:8080",
         "https://odoo-conector-fe.vercel.app",
         "https://odoo-conector-fe-murex.vercel.app",
-        "https://odoo-conector-fe-production.up.railway.app",
     ]  # todo: cambiar a la url del front
     if ENV == "LOCAL"
     else [
