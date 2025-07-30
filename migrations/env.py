f4d3914bd59bed17daa97fa4f69e18c0bd3311cd
fmancillageo <federico.mancilla@geonosis.com.ar>
2025-07-30 16:52:08 -0300

    Se agrega importación de TimesheetLineNotificationModel en el archivo de migraciones para integrar la nueva tabla de notificaciones en el esquema de la base de datos.

--- migrations/env.py
+++ migrations/env.py
@@ -6,6 +6,7 @@ from sqlmodel import SQLModel
 from alembic import context
 
 from app.users.infra.db.models import UserModel
+from app.timesheet_line.infra.db.models import TimesheetLineNotificationModel
 from app.auth.infra.db.models import (
     RefreshTokenModel,
     OTPModel,
